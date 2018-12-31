1e6e8b1117400e7b45846c14b1a7f9f7eede8f9b
buckle2000 <buckle2000@zoho.com>
2018-12-31 03:57:44 -0600

    Parse requirements from Pipfile.lock in setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
+import json
 from distutils.core import setup
+
+with open('Pipfile.lock') as f:
+    install_requires = [k + v['version'] for k,v in json.load(f)['default'].items()]
 
 setup(
     name='TTYles',
-    version='0.1.0',
+    version='0.1.1',
     author='buckle2000',
     # author_email='',
     packages=['ttyles'],
     # scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
-    # url='http://pypi.python.org/pypi/TTYles/',
+    url='https://github.com/buckle2000/ttyles/',
     license='LICENSE',
     description='Easy to use terminal manipulation library with a Pythonic interface.',
     long_description=open('README.md').read(),
-    install_requires=[
-        "blessed>=1.15.0",
-    ],
+    install_requires=install_requires,
 )
