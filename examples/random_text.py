2aed0954dbe0d689a204349182e1fede75449a53
buckle2000 <buckle2000@zoho.com>
2018-12-30 23:43:51 -0600

    Update example

--- examples/random_text.py
+++ examples/random_text.py
@@ -1,16 +1,26 @@
 from time import sleep
 from ttyles import Terminal
-from random import choice
-
-tty = Terminal.from_std() # create terminal with stdout
-
-width, height = 80, 1
-tty.size = (width, height) # set size of terminal
+from random import choice, randrange
 
 charset = '1234567890)(*&^%$#@!;:",./?><{}[]|'
 
+# create terminal with stdout
+tty = Terminal.from_std()
+
+# set size of terminal
+width, height = 80, 1
+tty.size = (width, height)
+
+# clear screen
+tty.clear()
+
+# print a bunch of random characters
+row = [choice(charset) for _ in range(width)]
+tty[0, 0] = ''.join(row)
+
 for i in range(100):
-    tty.reset() # reset (RIS) terminal
-    row = [choice(charset) for _ in range(width)]
-    tty[0, 0] = ''.join(row) # print text at (0, 0)
-    sleep(0.1)
+    # print random character at random position
+    tty[randrange(width), 0] = choice(charset)
+    sleep(0.01)
+
+tty.print('\n')
